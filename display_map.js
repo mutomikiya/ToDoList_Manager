42971e3eb9a20186917eb19a6f5dcd0a690c0b87
mutomikiya <muto.mikiya@goukaku-suppli.com>
2023-09-16 03:12:08 +0900

    色の変更

--- display_map.js
+++ display_map.js
@@ -1,34 +1,40 @@
-var width = 1500,
+var width = 1200,
     height = 800;
 var scale =10000;
 var pointdata = {
         "type":"LineString",
         "coordinates": []
     }
-    
+
 var elements = document.getElementById("plot-stations");
 for (const child of elements.children) {
     point = child.attributes.item(0).value.split(',');
     pointdata["coordinates"].push([Number.parseFloat(point[0]), Number.parseFloat(point[1])])
 }
+elements.remove();
 
 d3.json("japan_prefecture.geojson", createMap);
 
 function createMap(japan) {
     var aProjection = d3.geoMercator()
-        .center([ 139.0, 38.6 ])
+        .center([ 139.5, 38.6 ])
         .translate([width/2, height/2])
         .scale(scale);
     var geoPath = d3.geoPath().projection(aProjection);
     var svg = d3.select("svg").attr("width",width).attr("height",height);
     //マップ描画
+    var background = svg
+        .append("rect")
+        .attr("width",width)
+        .attr("height",height)
+        .attr("fill", "#a9ceec");
     var map = svg.selectAll("path").data(japan.features)
-    .enter()
-    .append("path")
-        .attr("d", geoPath)
-        .style("stroke", "#797979")
-        .style("stroke-width", 0.1)
-        .style("fill", "#797979");
+        .enter()
+        .append("path")
+            .attr("d", geoPath)
+            .style("stroke", "#797979")
+            .style("stroke-width", 0.1)
+            .style("fill", "#FFEDB3");
 
     var line = svg.selectAll(".ilne").data([pointdata])
         .enter()
